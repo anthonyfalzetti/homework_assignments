d7395976420e3c54f647357392092ad48d5be8d8
Anthony Falzetti <Anthony.falzetti@gmail.com>
2015-02-26 16:09:47 -0700

    Update the array_practice.rb
    I removed the extra array and instead ran all the methods on the existing one.
    I changed the .reject to delete_if and I changed the keep to a select so as not to change the array.
    I change dthe iterators from number to digit... so that I wasn't over the space limit :)
    
    [#88972898]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 starting_array = (1..10).to_a
-new_array = starting_array.reject { |number| [5, 6, 7].include?(number) }
 
 puts "#{starting_array.join('...')}..."
 puts "T-#{starting_array.reverse.join(', ')}...  BLASTOFF!"
@@ -11,12 +10,15 @@ puts "The third element is #{starting_array[2]}"
 puts "The element with an index of 3 is #{starting_array[3]}"
 puts "The second from last element is #{starting_array[-2]}"
 puts "The first four elements are '#{starting_array.first(4).join(', ')}'"
+starting_array.delete_if { |digit| [5, 6, 7].include?(digit) }
 print 'If we delete 5, 6 and 7 from the array,'
-puts " we're left with [#{new_array.join(',')}]"
+puts " we're left with [#{starting_array.join(',')}]"
+starting_array.unshift(5)
 print 'If we add 5 at the beginning of the array,'
-puts " we're left with [#{new_array.unshift(5).join(',')}] "
+puts " we're left with [#{starting_array.join(',')}] "
+starting_array.push(6)
 print 'If we add 6 at the end of the array,'
-puts " we're left with [#{new_array.push(6).join(',')}]"
-puts "Only the elements #{new_array.keep_if { |number| number > 8 }} are > 8."
+puts " we're left with [#{starting_array.join(',')}]"
+puts "Only the elements #{starting_array.select { |digit| digit > 8 }} are > 8."
 print 'If we remove all the elements,'
-puts " then the length of the array is #{new_array.clear.length}"
+puts " then the length of the array is #{starting_array.clear.length}"
